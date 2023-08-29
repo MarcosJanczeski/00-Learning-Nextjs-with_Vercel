31ffaa0ee7e08e5ab1a9274cf3451505f22474ba
Marcos Janczeski <marcosjanczeski@gmail.com>
2023-08-28 20:15:22 -0400

    ADICIONANDO ESTILIZAÇÃO COM STYLE JSX

--- pages/posts/[slug].jsx
+++ pages/posts/[slug].jsx
@@ -1,14 +1,15 @@
 import { useRouter } from "next/router"
 import posts from "./posts"
 import Link from "next/link"
+import Layout from "../../components/layout"
 
 export default function Post() {
   const post = posts.find(post => post.slug === useRouter().query.slug)
-  return <>
+  return <Layout>
     <h3>{post.title}</h3>
     <p>{post.content}</p>
     <small>{post.author}</small><br /><br />
     <Link href='/posts'>BACK TO POSTS</Link><br /><br />
     <Link href='/'>BACK TO HOME</Link>
-  </>
+  </Layout>
 }
